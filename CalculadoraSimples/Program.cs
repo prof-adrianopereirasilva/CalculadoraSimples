e9c613e678ae7075f7e3181f9543247329e5d2f1
prof-adrianopereirasilva <prof.adrianopereirasilva@gmail.com>
2024-04-24 17:34:13 -0300

    CalculadoraSimples

---
+++ CalculadoraSimples/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CalculadoraSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Para fazer o calculo \n digite o primeiro numero,  depois o segundo numero \n e por ultimo escolha a operação a ser executada. ");
+            
+             Console.WriteLine("\n*************************************** \n ");
+            
+            Console.WriteLine("Digite o primeiro número: ");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+
+                       
+
+
+            Console.WriteLine("Digite o segundo número: ");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Selecione a operação: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+
+            int operacao = Convert.ToInt32(Console.ReadLine());
+            int resultado = 0;
+
+            switch (operacao)
+            {
+                case 1:
+                    resultado = numero1 + numero2;
+                    break;
+                case 2:
+                    resultado = numero1 - numero2;
+                    break;
+                case 3:
+                    resultado = numero1 * numero2;
+                    break;
+                case 4:
+                    resultado = numero1 / numero2;
+                    break;
+                default:
+                    Console.WriteLine("Operação inválida.");
+                    break;
+            }
+
+            Console.WriteLine("O resultado é: " + resultado);
+        }
+    }
+}
